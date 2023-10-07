2ffc45907b7078230d05d4138423c1a4229aafb5
hussainsan <hussain.almahmud1@outlook.com>
2023-10-07 13:47:28 +0900

    Add untracked files

---
+++ config/my_models/DeepLabV3Plus_efb2.py
@@ -0,0 +1,12 @@
+config = dict(
+    encoder='timm-efficientnet-b2',
+    model_network='DeepLabV3Plus',
+    in_channels=3,
+    n_class=1,
+    save_path='',
+    learning_rate = 0.0003,
+    weight_decay = 0.0005,
+    seed=10000,
+    fold_n=1,
+)
+config['save_path']='{}_{}'.format(config['encoder'],config['model_network'])
---
+++ config/my_models/UnetPlusPlus_efb3.py
@@ -0,0 +1,11 @@
+config = dict(
+    encoder='timm-efficientnet-b3',
+    model_network='UnetPlusPlus',
+    in_channels=3,
+    n_class=1,
+    save_path='',
+    learning_rate = 0.0005,
+    weight_decay = 0.0005,
+    seed=10000,
+)
+config['save_path']='{}_{}'.format(config['encoder'],config['model_network'])
